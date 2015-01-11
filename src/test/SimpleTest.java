bdd3e8daefd284a88ea42e8c980000291336daf7
NicolasGriere <griere@ece.fr>
2015-01-11 23:02:55 +0100

    Multi User attempt

--- src/test/SimpleTest.java
+++ src/test/SimpleTest.java
@@ -10,9 +10,9 @@ import bank.Operation;
 
 /**
  * The squeleton of a simple test program for {@link BankManagerImpl}.
- * 
+ *
  * @author Busca
- * 
+ *
  */
 public class SimpleTest {
 
@@ -33,23 +33,38 @@ public class SimpleTest {
     //
     static class CustomerEmulator extends Thread {
 
-	private BankManager manager;
-	private String customer;
+        private BankManager manager;
+        private String customer;
 
-	public CustomerEmulator(BankManager m, String c) {
-	    manager = m;
-	    customer = c;
-	}
+        public CustomerEmulator(BankManager m, String c) {
+            manager = m;
+            customer = c;
+        }
 
-	public String toString() {
-	    return customer + "[" + manager + "]";
-	}
+        public String toString() {
+            return customer + "[" + manager + "]";
+        }
 
-	public void run() {
-	    System.out.println(this + ": starting");
-	    // TODO complete the test
-	    System.out.println(this + ": exiting");
-	}
+        public void run() {
+            System.out.println(this + ": starting");
+
+            // Analyse the customer and test different posibilities
+            switch (customer) {
+                case "customer1":
+                    // deposit 1000 on account #1
+                    double b = manager.addBalance(1, 1000.0);
+                    
+                    break;
+                case "customer2":
+                    break;
+                case "customer3":
+                    break;
+                case "customer4":
+                    break;
+            }
+
+            System.out.println(this + ": exiting");
+        }
 
     }
 
@@ -57,39 +72,38 @@ public class SimpleTest {
     // HELPER METHODS
     //
     private static void check(String test, boolean ok) {
-	testTotal += 1;
-	System.out.print(test + ": ");
-	if (ok) {
-	    testOK += 1;
-	    System.out.println("ok");
-	} else {
-	    System.out.println("FAILED");
-	}
+        testTotal += 1;
+        System.out.print(test + ": ");
+        if (ok) {
+            testOK += 1;
+            System.out.println("ok");
+        } else {
+            System.out.println("FAILED");
+        }
     }
 
     private static void singleUserTests(BankManager m, String c) throws SQLException {
 
-	// deposit 1000 on account #1
-	double b = m.addBalance(1, 1000.0);
-	check("addBalance",  b == 1000.0);
+        // deposit 1000 on account #1
+        double b = m.addBalance(1, 1000.0);
+        check("addBalance", b == 1000.0);
 
-	// transfert 500 from account #1 to account #2
-	boolean s = m.transfer(1, 2, 250.0);
-	check("transfert-1",  s);
-	check("transfert-2",  m.getBalance(1) == 750.0);
-	check("transfert-3",  m.getBalance(2) == 250.0);
-	
-	// check operations on account #1 between yesterday and now
-	Date now = new Date();
-	List<Operation> o1 = m.getOperations(1, new Date(now.getTime() - 24*60*60*1000), now);
-	System.out.println("operations on account #1 = " + o1);
-	check("getOperations-1", o1.size() == 2);
-	List<Operation> o2 = m.getOperations(2, new Date(now.getTime() - 24*60*60*1000), now);
-	System.out.println("operations on account #2 = " + o2);
-	check("getOperations-1", o2.size() == 1);
-	
+        // transfert 500 from account #1 to account #2
+        boolean s = m.transfer(1, 2, 250.0);
+        check("transfert-1", s);
+        check("transfert-2", m.getBalance(1) == 750.0);
+        check("transfert-3", m.getBalance(2) == 250.0);
+
+        // check operations on account #1 between yesterday and now
+        Date now = new Date();
+        List<Operation> o1 = m.getOperations(1, new Date(now.getTime() - 24 * 60 * 60 * 1000), now);
+        System.out.println("operations on account #1 = " + o1);
+        check("getOperations-1", o1.size() == 2);
+        List<Operation> o2 = m.getOperations(2, new Date(now.getTime() - 24 * 60 * 60 * 1000), now);
+        System.out.println("operations on account #2 = " + o2);
+        check("getOperations-1", o2.size() == 1);
+
 	// TODO complete the test
-	
     }
 
     //
@@ -99,48 +113,44 @@ public class SimpleTest {
 
 	// check parameters
 	/*if (args.length != 3) {
-	    System.err.println("usage: SimpleTest <url> <user> <password>");
-	    System.exit(-1);
-	}*/
+         System.err.println("usage: SimpleTest <url> <user> <password>");
+         System.exit(-1);
+         }*/
+        try {
+            // create ReservationManager object
+            BankManager manager = new BankManagerImpl(args[0], args[1], "");
 
-	try {
-	    // create ReservationManager object
-	    BankManager manager = new BankManagerImpl(args[0], args[1], "");
+            // create the database
+            manager.createDB();
 
-	    // create the database
-	    manager.createDB();
+            // populate the database
+            for (int i = 0; i < MAX_ACCOUNTS; i++) {
+                manager.createAccount(i + 1);
+            }
 
-            
+            // execute single-user tests
+            singleUserTests(manager, "single-customer");
 
+            // execute multi-user tests
+            for (int i = 0; i < MAX_CUSTOMERS; i++) {
+                BankManager m = new BankManagerImpl(args[0], args[1], "");
+                new CustomerEmulator(m, "multi-customer" + i).start();
+            }
 
-	    // populate the database
-	    for (int i = 0; i < MAX_ACCOUNTS; i++) {
-		manager.createAccount(i + 1);
-	    }
+        } catch (Exception e) {
+            System.err.println("test aborted: " + e);
+            e.printStackTrace();
+        }
 
-	    // execute single-user tests
-	    singleUserTests(manager, "single-customer");
-
-	    // execute multi-user tests
-	    for (int i = 0; i < MAX_CUSTOMERS; i++) {
-		BankManager m = new BankManagerImpl(args[0], args[1], args[2]);
-		new CustomerEmulator(m, "multi-customer" + i).start();
-	    }
-
-	} catch (Exception e) {
-	    System.err.println("test aborted: " + e);
-	    e.printStackTrace();
-	}
-
-	// print test results
-	if (testTotal == 0) {
-	    System.out.println("no test performed");
-	} else {
-	    String r = "test results: ";
-	    r += "total=" + testTotal;
-	    r += ", ok=" + testOK + "(" + ((testOK * 100) / testTotal) + "%)";
-	    System.out.println(r);
-	}
+        // print test results
+        if (testTotal == 0) {
+            System.out.println("no test performed");
+        } else {
+            String r = "test results: ";
+            r += "total=" + testTotal;
+            r += ", ok=" + testOK + "(" + ((testOK * 100) / testTotal) + "%)";
+            System.out.println(r);
+        }
 
     }
 }
